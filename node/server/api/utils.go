e85099c61e51f0d27d9aa3c65472dec5a46a0def
DimitrisJim <d.f.hilliard@gmail.com>
2024-02-29 00:25:42 +0200

    nit: Add utils.go, move EngineForkStates to it, add stub/docs for IsIBCBridgeTx.

---
+++ node/server/api/utils.go
@@ -0,0 +1,31 @@
+package api
+
+import "github.com/ethereum-optimism/optimism/op-service/eth"
+
+const ibcBridgeAddress = "0x42000000000000000000000000000000000000E1"
+
+// EngineForkStates takes in the interceptor fork state and the blockstore and creates two states for abci and
+// geth.
+func EngineForkStates(blockStore BlockStore, interceptorForkState eth.ForkchoiceState) (eth.ForkchoiceState, eth.ForkchoiceState) {
+	head, safe, finalized := interceptorForkState.HeadBlockHash, interceptorForkState.SafeBlockHash, interceptorForkState.FinalizedBlockHash
+
+	abci := eth.ForkchoiceState{
+		HeadBlockHash:      blockStore.GetCompositeBlock(head).ABCIHash,
+		SafeBlockHash:      blockStore.GetCompositeBlock(safe).ABCIHash,
+		FinalizedBlockHash: blockStore.GetCompositeBlock(finalized).ABCIHash,
+	}
+
+	geth := eth.ForkchoiceState{
+		HeadBlockHash:      blockStore.GetCompositeBlock(head).GethHash,
+		SafeBlockHash:      blockStore.GetCompositeBlock(safe).GethHash,
+		FinalizedBlockHash: blockStore.GetCompositeBlock(finalized).GethHash,
+	}
+
+	return abci, geth
+}
+
+// IsIBCBridgeTx returns true if the transaction is a transaction sent to the IBCStandardBridge.
+// tx is a hex encoded marshalled types.Transaction (op-geth/core/types/transaction.go)
+func IsIBCBridgeTx(tx []byte) bool {
+	return false
+}
